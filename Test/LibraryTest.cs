917fae4b5c53daccdcaf77d206e96b36e3804192
Mikhail Kita <mihail.kita@gmail.com>
2015-11-11 22:44:47 +0300

    Test library rewritten on C#

---
+++ Test/LibraryTest.cs
@@ -0,0 +1,21 @@
+﻿using GeomLibrary;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class LibraryTest
+    {
+        [TestCase(new float[4] { 1f, 2f, 2f, 1f }, new float[4] { 2f, 2f, 1f, 1f }, Result = "This is a convex quadrangle")]
+        [TestCase(new float[4] { -2f, 0f, 2f, 0f }, new float[4] { 0f, 2f, 0f, -2f }, Result = "This is a convex quadrangle")]
+        [TestCase(new float[4] { -3f, 1f, 0f, 1f }, new float[4] { 1f, 1f, 0f, -1f }, Result = "This is not a convex quadrangle")]
+        [TestCase(new float[4] { -1f, 0f, 1f, 1f }, new float[4] { 2f, 0f, -2f, 1f }, Result = "This is a triangle")]
+        [TestCase(new float[4] { -1f, 0f, 1f, 2f }, new float[4] { -1f, 0f, 1f, 2f }, Result = "This is a line")]
+        [TestCase(new float[4] { 1f, 1f, 1f, 1f }, new float[4] { 2f, 2f, 2f, 2f }, Result = "This is a dot")]
+        public string test(float[] x, float[] y)
+        {
+            Library lib = new Library();
+            return lib.result(x, y);
+        }
+    }
+}
