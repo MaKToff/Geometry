b2039a42f1fee369f5cb7a9d244cd3d0952166c1
Mikhail Kita <mihail.kita@gmail.com>
2015-11-16 11:59:19 +0300

    Fixed resizing of window

--- GUI/GUI.Designer.cs
+++ GUI/GUI.Designer.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
 
 namespace GUI
 {
@@ -7,7 +10,7 @@ namespace GUI
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,7 +31,7 @@ namespace GUI
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(GUI));
             inputGroupBox = new GroupBox();
             pointDy = new TextBox();
             pointDx = new TextBox();
@@ -53,8 +56,8 @@ namespace GUI
             resultTextBox = new TextBox();
             scaleGroupBox = new GroupBox();
             inputGroupBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(pictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(trackBar)).BeginInit();
+            ((ISupportInitialize)(pictureBox)).BeginInit();
+            ((ISupportInitialize)(trackBar)).BeginInit();
             menuStrip.SuspendLayout();
             resultGroupBox.SuspendLayout();
             scaleGroupBox.SuspendLayout();
@@ -74,89 +77,89 @@ namespace GUI
             inputGroupBox.Controls.Add(labelC);
             inputGroupBox.Controls.Add(labelB);
             inputGroupBox.Controls.Add(labelA);
-            inputGroupBox.Location = new System.Drawing.Point(453, 53);
+            inputGroupBox.Location = new Point(453, 53);
             inputGroupBox.Name = "inputGroupBox";
-            inputGroupBox.Size = new System.Drawing.Size(234, 231);
+            inputGroupBox.Size = new Size(234, 231);
             inputGroupBox.TabIndex = 0;
             inputGroupBox.TabStop = false;
             inputGroupBox.Text = "Input data";
             // 
             // pointDy
             // 
-            pointDy.Location = new System.Drawing.Point(155, 190);
+            pointDy.Location = new Point(155, 190);
             pointDy.MaxLength = 5;
             pointDy.Name = "pointDy";
-            pointDy.Size = new System.Drawing.Size(62, 26);
+            pointDy.Size = new Size(62, 26);
             pointDy.TabIndex = 13;
             pointDy.Text = "0";
             pointDy.TextAlign = HorizontalAlignment.Center;
             // 
             // pointDx
             // 
-            pointDx.Location = new System.Drawing.Point(74, 190);
+            pointDx.Location = new Point(74, 190);
             pointDx.MaxLength = 5;
             pointDx.Name = "pointDx";
-            pointDx.Size = new System.Drawing.Size(62, 26);
+            pointDx.Size = new Size(62, 26);
             pointDx.TabIndex = 12;
             pointDx.Text = "0";
             pointDx.TextAlign = HorizontalAlignment.Center;
             // 
             // pointCy
             // 
-            pointCy.Location = new System.Drawing.Point(155, 140);
+            pointCy.Location = new Point(155, 140);
             pointCy.MaxLength = 5;
             pointCy.Name = "pointCy";
-            pointCy.Size = new System.Drawing.Size(62, 26);
+            pointCy.Size = new Size(62, 26);
             pointCy.TabIndex = 11;
             pointCy.Text = "0";
             pointCy.TextAlign = HorizontalAlignment.Center;
             // 
             // pointCx
             // 
-            pointCx.Location = new System.Drawing.Point(74, 140);
+            pointCx.Location = new Point(74, 140);
             pointCx.MaxLength = 5;
             pointCx.Name = "pointCx";
-            pointCx.Size = new System.Drawing.Size(62, 26);
+            pointCx.Size = new Size(62, 26);
             pointCx.TabIndex = 10;
             pointCx.Text = "0";
             pointCx.TextAlign = HorizontalAlignment.Center;
             // 
             // pointBy
             // 
-            pointBy.Location = new System.Drawing.Point(155, 90);
+            pointBy.Location = new Point(155, 90);
             pointBy.MaxLength = 5;
             pointBy.Name = "pointBy";
-            pointBy.Size = new System.Drawing.Size(62, 26);
+            pointBy.Size = new Size(62, 26);
             pointBy.TabIndex = 9;
             pointBy.Text = "0";
             pointBy.TextAlign = HorizontalAlignment.Center;
             // 
             // pointBx
             // 
-            pointBx.Location = new System.Drawing.Point(74, 90);
+            pointBx.Location = new Point(74, 90);
             pointBx.MaxLength = 5;
             pointBx.Name = "pointBx";
-            pointBx.Size = new System.Drawing.Size(62, 26);
+            pointBx.Size = new Size(62, 26);
             pointBx.TabIndex = 8;
             pointBx.Text = "0";
             pointBx.TextAlign = HorizontalAlignment.Center;
             // 
             // pointAy
             // 
-            pointAy.Location = new System.Drawing.Point(155, 40);
+            pointAy.Location = new Point(155, 40);
             pointAy.MaxLength = 5;
             pointAy.Name = "pointAy";
-            pointAy.Size = new System.Drawing.Size(62, 26);
+            pointAy.Size = new Size(62, 26);
             pointAy.TabIndex = 7;
             pointAy.Text = "0";
             pointAy.TextAlign = HorizontalAlignment.Center;
             // 
             // pointAx
             // 
-            pointAx.Location = new System.Drawing.Point(74, 40);
+            pointAx.Location = new Point(74, 40);
             pointAx.MaxLength = 5;
             pointAx.Name = "pointAx";
-            pointAx.Size = new System.Drawing.Size(62, 26);
+            pointAx.Size = new Size(62, 26);
             pointAx.TabIndex = 6;
             pointAx.Text = "0";
             pointAx.TextAlign = HorizontalAlignment.Center;
@@ -164,108 +167,108 @@ namespace GUI
             // labelD
             // 
             labelD.AutoSize = true;
-            labelD.Location = new System.Drawing.Point(14, 193);
+            labelD.Location = new Point(14, 193);
             labelD.Name = "labelD";
-            labelD.Size = new System.Drawing.Size(21, 20);
+            labelD.Size = new Size(21, 20);
             labelD.TabIndex = 3;
             labelD.Text = "D";
             // 
             // labelC
             // 
             labelC.AutoSize = true;
-            labelC.Location = new System.Drawing.Point(15, 143);
+            labelC.Location = new Point(15, 143);
             labelC.Name = "labelC";
-            labelC.Size = new System.Drawing.Size(20, 20);
+            labelC.Size = new Size(20, 20);
             labelC.TabIndex = 2;
             labelC.Text = "C";
             // 
             // labelB
             // 
             labelB.AutoSize = true;
-            labelB.Location = new System.Drawing.Point(15, 93);
+            labelB.Location = new Point(15, 93);
             labelB.Name = "labelB";
-            labelB.Size = new System.Drawing.Size(20, 20);
+            labelB.Size = new Size(20, 20);
             labelB.TabIndex = 1;
             labelB.Text = "B";
             // 
             // labelA
             // 
             labelA.AutoSize = true;
-            labelA.Location = new System.Drawing.Point(15, 43);
+            labelA.Location = new Point(15, 43);
             labelA.Name = "labelA";
-            labelA.Size = new System.Drawing.Size(20, 20);
+            labelA.Size = new Size(20, 20);
             labelA.TabIndex = 0;
             labelA.Text = "A";
             // 
             // pictureBox
             // 
-            pictureBox.Location = new System.Drawing.Point(12, 53);
+            pictureBox.Location = new Point(12, 53);
             pictureBox.Name = "pictureBox";
-            pictureBox.Size = new System.Drawing.Size(425, 425);
+            pictureBox.Size = new Size(425, 425);
             pictureBox.TabIndex = 1;
             pictureBox.TabStop = false;
             // 
             // clearButton
             // 
-            clearButton.Location = new System.Drawing.Point(453, 414);
+            clearButton.Location = new Point(453, 414);
             clearButton.Name = "clearButton";
-            clearButton.Size = new System.Drawing.Size(107, 64);
+            clearButton.Size = new Size(107, 64);
             clearButton.TabIndex = 2;
             clearButton.Text = "Clear";
             clearButton.UseVisualStyleBackColor = true;
-            clearButton.Click += new System.EventHandler(clearButton_Click);
+            clearButton.Click += new EventHandler(clearButton_Click);
             // 
             // startButton
             // 
-            startButton.Location = new System.Drawing.Point(578, 416);
+            startButton.Location = new Point(578, 416);
             startButton.Name = "startButton";
-            startButton.Size = new System.Drawing.Size(109, 64);
+            startButton.Size = new Size(109, 64);
             startButton.TabIndex = 3;
             startButton.Text = "Start";
             startButton.UseVisualStyleBackColor = true;
-            startButton.Click += new System.EventHandler(startButton_Click);
+            startButton.Click += new EventHandler(startButton_Click);
             // 
             // trackBar
             // 
-            trackBar.Location = new System.Drawing.Point(6, 25);
+            trackBar.Location = new Point(6, 25);
             trackBar.Minimum = 1;
             trackBar.Name = "trackBar";
-            trackBar.Size = new System.Drawing.Size(222, 69);
+            trackBar.Size = new Size(222, 69);
             trackBar.TabIndex = 16;
             trackBar.Value = 5;
-            trackBar.Scroll += new System.EventHandler(trackBar_Scroll);
+            trackBar.Scroll += new EventHandler(trackBar_Scroll);
             // 
             // fileToolStripMenuItem
             // 
             fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] {
             exitToolStripMenuItem});
             fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            fileToolStripMenuItem.Size = new Size(50, 29);
             fileToolStripMenuItem.Text = "File";
             // 
             // exitToolStripMenuItem
             // 
             exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new System.Drawing.Size(124, 30);
+            exitToolStripMenuItem.Size = new Size(124, 30);
             exitToolStripMenuItem.Text = "Exit";
-            exitToolStripMenuItem.Click += new System.EventHandler(exitToolStripMenuItem_Click);
+            exitToolStripMenuItem.Click += new EventHandler(exitToolStripMenuItem_Click);
             // 
             // menuStrip
             // 
-            menuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            menuStrip.ImageScalingSize = new Size(24, 24);
             menuStrip.Items.AddRange(new ToolStripItem[] {
             fileToolStripMenuItem});
-            menuStrip.Location = new System.Drawing.Point(0, 0);
+            menuStrip.Location = new Point(0, 0);
             menuStrip.Name = "menuStrip";
-            menuStrip.Size = new System.Drawing.Size(701, 33);
+            menuStrip.Size = new Size(701, 33);
             menuStrip.TabIndex = 5;
             // 
             // resultGroupBox
             // 
             resultGroupBox.Controls.Add(resultTextBox);
-            resultGroupBox.Location = new System.Drawing.Point(12, 497);
+            resultGroupBox.Location = new Point(12, 497);
             resultGroupBox.Name = "resultGroupBox";
-            resultGroupBox.Size = new System.Drawing.Size(675, 79);
+            resultGroupBox.Size = new Size(675, 79);
             resultGroupBox.TabIndex = 6;
             resultGroupBox.TabStop = false;
             resultGroupBox.Text = "Result";
@@ -273,26 +276,26 @@ namespace GUI
             // resultTextBox
             // 
             resultTextBox.Enabled = false;
-            resultTextBox.Location = new System.Drawing.Point(15, 36);
+            resultTextBox.Location = new Point(15, 36);
             resultTextBox.Name = "resultTextBox";
-            resultTextBox.Size = new System.Drawing.Size(643, 26);
+            resultTextBox.Size = new Size(643, 26);
             resultTextBox.TabIndex = 0;
             // 
             // scaleGroupBox
             // 
             scaleGroupBox.Controls.Add(trackBar);
-            scaleGroupBox.Location = new System.Drawing.Point(455, 299);
+            scaleGroupBox.Location = new Point(455, 299);
             scaleGroupBox.Name = "scaleGroupBox";
-            scaleGroupBox.Size = new System.Drawing.Size(234, 100);
+            scaleGroupBox.Size = new Size(234, 100);
             scaleGroupBox.TabIndex = 17;
             scaleGroupBox.TabStop = false;
             scaleGroupBox.Text = "Scale";
             // 
             // GUI
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(701, 588);
+            ClientSize = new Size(701, 588);
             Controls.Add(scaleGroupBox);
             Controls.Add(resultGroupBox);
             Controls.Add(menuStrip);
@@ -300,14 +303,18 @@ namespace GUI
             Controls.Add(clearButton);
             Controls.Add(pictureBox);
             Controls.Add(inputGroupBox);
-            Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            Icon = ((Icon)(resources.GetObject("$this.Icon")));
             MainMenuStrip = menuStrip;
+            MaximizeBox = false;
+            MaximumSize = new Size(723, 644);
+            MinimizeBox = false;
+            MinimumSize = new Size(723, 644);
             Name = "GUI";
             Text = "Geometry";
             inputGroupBox.ResumeLayout(false);
             inputGroupBox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(pictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(trackBar)).EndInit();
+            ((ISupportInitialize)(pictureBox)).EndInit();
+            ((ISupportInitialize)(trackBar)).EndInit();
             menuStrip.ResumeLayout(false);
             menuStrip.PerformLayout();
             resultGroupBox.ResumeLayout(false);
